1027e91048489044ee06e1eea5c8bd50e29924bd
PRAJAPATI <PRAJAPATI@DESKTOP-AUIJI1A>
2020-05-05 19:58:03 +0530

    New Code Setup

---
+++ DemoSkillApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DemoSkillApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Demo Skill App");
+            Console.ReadLine();
+        }
+    }
+}
